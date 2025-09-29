ff5fac16a292da41562d67863d972c4f26b0ebe4
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-29 14:13:49 +0000

    mensagem

---
+++ item.py
@@ -0,0 +1,17 @@
+class Item:
+    def __init__(self, id, descricao, quantidade):
+        self.__id = 0
+        self.__descricao = ""
+        self.__quantidade = 0
+    def set_id(self, id):
+        self.__id = id
+    def set_descricao(self, descricao):
+        self.__descricao = descricao
+    def set_quantidade(self, quantidade):
+        self.__quantidade = quantidade
+    def get_id(self):
+        return id
+    def get_descricao(self):
+        return descricao
+    def get_quantidade(self):
+        return quantidade
