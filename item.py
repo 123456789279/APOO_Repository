e6e3414496ebdd83e438ba34de98cae0c14b6591
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-29 14:19:06 +0000

    mensagem

--- item.py
+++ item.py
@@ -9,11 +9,17 @@ class Item:
         self.__descricao = descricao
     def set_quantidade(self, quantidade):
         self.__quantidade = quantidade
-    def get_id(self):
+    def get_id(self, id):
         return id
-    def get_descricao(self):
+    def get_descricao(self, descricao):
         return descricao
-    def get_quantidade(self):
+    def get_quantidade(self, quantidade):
         return quantidade
     def __str__(self):
         return f"{self.__id}-{self.__descricao}-{self.__quantidade}"
+    def to_json(self):
+        dic = {"id":self.__id, "descricao":self.__descricao,"quantidade":self.__quantidade}
+        return dic
+    @staticmethod
+    def from_json(dic):
+        return Item(dic["id"], dic["descricao"], dic["quantidade"])
