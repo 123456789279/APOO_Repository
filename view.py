86938293492536f944b1cba438cde16b0d5a196f
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-29 14:32:30 +0000

    mensagem

--- view.py
+++ view.py
@@ -1,18 +1,18 @@
 from item import Item, ItemDAO
 class View:
-   def item_inserir(data, confirmado, id_cliente, id_servico):
+   def item_inserir(data, confirmado, id_descricao, id_quantidade):
      c = Item(0, data)
      c.set_confirmado(confirmado)
-     c.set_id_cliente(id_cliente)
-     c.set_id_servico(id_servico)
+     c.set_id_descricao(id_descricao)
+     c.set_id_quantidade(id_quantidade)
      ItemDAO.inserir(c)
    def item_listar():
      return ItemDAO.listar()
-   def item_atualizar(id, data, confirmado, id_cliente, id_servico):
+   def item_atualizar(id, data, confirmado, id_descricao, id_quantidade):
      c = Item(id, data)
      c.set_confirmado(confirmado)
-     c.set_id_cliente(id_cliente)
-     c.set_id_servico(id_servico)
+     c.set_id_descricao(id_descricao)
+     c.set_id_quantidade(id_quantidade)
      ItemDAO.atualizar(c)
    def item_excluir(id):
      c = Item(id, None)
